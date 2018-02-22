b4b1697a0895bb48d806d5883ffd2c557c39d83a
bizworld <bizworld@outlook.com>
2018-02-22 16:42:31 -0500

    Pass data through props by passing some data from the Board component to the Square component.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ class Square extends React.Component {
     return (
       <button className="square">
         {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -14,7 +15,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
